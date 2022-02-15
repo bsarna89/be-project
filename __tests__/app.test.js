b4b1f044607e3c0897522cfda07bc314af9d6417
Bartosz <bsarna89@gmail.com>
2022-02-15 11:42:03 +0000

    small mistake

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -11,7 +11,7 @@ afterAll(() => {
 });
 
 describe('/api/topics', () => {
-    describe('GET', () => {
+    describe('GET topics', () => {
         test('/api/topics returns array of objects, should have slug and description property ', () => {
             return request(app).get('/api/topics').expect(200).then((response) => {
 
@@ -40,8 +40,59 @@ describe('/api/topics', () => {
         })
 
 
+    });
 
+});
 
+describe('/api/articles/:article_id', () => {
+    describe('GET article_id', () => {
+        test('/api/articles/:article_id,responds 200 with one element array containing article  ', () => {
+            return request(app).get('/api/articles/2').expect(200).then((response) => {
+
+                expect(response.body.article).toBeInstanceOf(Array);
+                expect(response.body.article.length).toBe(1);
+
+                response.body.article.forEach((article) => {
+                    expect(article).toEqual(expect.objectContaining({
+                        article_id: expect.any(Number),
+                        votes: expect.any(Number),
+                        title: expect.any(String),
+                        body: expect.any(String),
+                        created_at: expect.any(String),
+                        author: expect.any(String),
+                        topic: expect.any(String)
+                    }))
+                })
+
+            })
+
+        })
+
+        test('/api/articles/article:id responds error 404 when wrong path been passed ', () => {
+            return request(app).get('/api/tarticless').expect(404).then((response) => {
+
+                const message = { msg: "Path not found" };
+                expect(response.body).toEqual(message);
+            })
+
+        })
+
+        test('/api/articles/article:id responds error 404 when article_id does not exist in DB', () => {
+            return request(app).get('/api/articles/1234').expect(404).then((response) => {
+
+                const message = { msg: "Resource not found" };
+                expect(response.body).toEqual(message);
+            })
+
+        })
+        test('/api/articles/article:id responds error 400 when article_id is not valid', () => {
+            return request(app).get('/api/articles/ban').expect(400).then((response) => {
+
+                const message = { msg: "Bad Request" };
+                expect(response.body).toEqual(message);
+            })
+
+        })
     });
 
 });
